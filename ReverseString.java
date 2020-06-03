c644ea3e5ee61ca3794fc2008c2dc9398215c00e
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2020-06-03 09:16:39 +0530

    Reverse String Using Recursion

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+class ReverseString {
+  public static void main(String[] args) {
+    reverse("ab");
+  }
+
+  public static void reverse(String str)
+  {
+    if(str == null || str.length() <=1)
+      System.out.println(str);
+    else
+    {
+      System.out.print(str.charAt(str.length()-1));
+      reverse(str.substring(0,str.length()-1));
+    }
+  }
+
+}
